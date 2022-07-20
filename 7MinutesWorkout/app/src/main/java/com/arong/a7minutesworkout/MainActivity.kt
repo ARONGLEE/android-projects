4bbf9a38acbe283ffa4dc9a2e383d60bdb779312
AYOUNGLEE <74637336+ARONGLEE@users.noreply.github.com>
2022-07-20 16:32:56 +0900

    7MinutesWorkout upload

---
+++ 7MinutesWorkout/app/src/main/java/com/arong/a7minutesworkout/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.arong.a7minutesworkout
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.arong.a7minutesworkout.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+//        val flStartButton: FrameLayout = findViewById(R.id.fl_start)
+        binding?.flStart?.setOnClickListener {
+            val intent = Intent(this, ExerciseActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
